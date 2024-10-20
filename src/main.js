581f292ec1983870588f60ad136acf373300e928
José Antonio Díaz Miralles <tiyujopite1@gmail.com>
2024-10-20 20:39:06 +0200

    Remove coments.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-// import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createGettext } from 'vue3-gettext'
-// import { Vue3Mq } from 'vue3-mq'
 import './index.css'
 import translations from './assets/translations.json'
 
@@ -15,21 +13,11 @@ const gettext = createGettext({
   availableLanguages: {
     en: 'English',
     es: 'Español',
+    fr: 'Français',
   },
   defaultLanguage: 'en',
   translations: translations,
 })
-
 app.use(gettext)
 
-// app.use(Vue3Mq, {
-//   breakpoints: {
-//     phone: 0,
-//     tablet: 768,
-//     desktop: 1370,
-//   }
-// })
-
-
 app.mount('#app')
-// import 'bootstrap/dist/js/bootstrap.js'
